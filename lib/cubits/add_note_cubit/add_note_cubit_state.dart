ab0ff711cdcb86c4bd483c4ca9317a5cf15a9966
fadelnajem <fadel.almadar@gmail.com>
2022-12-15 14:17:59 +0200

    add to add note cubit state

--- lib/cubits/add_note_cubit/add_note_cubit_state.dart
+++ lib/cubits/add_note_cubit/add_note_cubit_state.dart
@@ -4,3 +4,13 @@ part of 'add_note_cubit_cubit.dart';
 abstract class AddNoteCubitState {}
 
 class AddNoteCubitInitial extends AddNoteCubitState {}
+
+class AddNoteLoading extends AddNoteCubitState {}
+
+class AddNoteSuccess extends AddNoteCubitState {}
+
+class AddNoteFailure extends AddNoteCubitState {
+  final String errMessage;
+
+  AddNoteFailure(this.errMessage);
+}
