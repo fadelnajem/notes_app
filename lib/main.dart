d4c2dbc67019b239ebd55a0615f6393c1f0a6442
fadelnajem <fadel.almadar@gmail.com>
2022-12-19 12:32:21 +0200

    trigger add note cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
 import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/constants.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit_cubit.dart';
@@ -11,7 +13,8 @@ import 'models/note_model.dart';
 void main() async {
   await Hive.initFlutter();
   Bloc.observer = SimpleBlocObserve();
-  await Hive.openBox(kNoteBox);
+  await Hive.openBox<NoteModel>(kNoteBox);
+
   Hive.registerAdapter(NoteModelAdapter());
   runApp(const NotesApp());
 }
