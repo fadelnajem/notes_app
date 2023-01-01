e5cb0e0b4fb40a5f8a4d945789ac40f06fbe089e
fadelnajem <fadel.almadar@gmail.com>
2023-01-01 13:45:04 +0200

    display data part two

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/widgets/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
-
+  const NoteItem({super.key, required this.note});
+  final NoteModel note;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -18,7 +19,7 @@ class NoteItem extends StatelessWidget {
       child: Container(
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(16),
-          color: const Color(0xffFFCC80),
+          color:  Color(note.color),
         ),
         child: Padding(
           padding: const EdgeInsets.only(top: 16, bottom: 16, left: 16),
@@ -26,9 +27,9 @@ class NoteItem extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.end,
             children: [
               ListTile(
-                title: const Text(
-                  'Flutter Tips',
-                  style: TextStyle(
+                title: Text(
+                  note.title,
+                  style: const TextStyle(
                     color: Colors.black,
                     fontSize: 26,
                   ),
@@ -36,7 +37,7 @@ class NoteItem extends StatelessWidget {
                 subtitle: Padding(
                   padding: const EdgeInsets.only(top: 16),
                   child: Text(
-                    'Build your career with Fadel Najem',
+                    note.subTitle,
                     style: TextStyle(
                       color: Colors.black.withOpacity(0.4),
                       fontSize: 18,
@@ -55,7 +56,7 @@ class NoteItem extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.only(top: 16, right: 24),
                 child: Text(
-                  'Dec12 , 2022',
+                  note.date,
                   style: TextStyle(
                     color: Colors.black.withOpacity(0.4),
                   ),
