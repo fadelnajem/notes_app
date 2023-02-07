d892815582f9cc814f0189000656a537f452613a
fadelnajem <fadel.almadar@gmail.com>
2023-02-07 15:36:50 +0200

    create custom app bar

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,9 @@ class NotesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(brightness: Brightness.dark),
       home: const NotesView(),
     );
   }
 }
-
--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,11 +1,14 @@
 
 import 'package:flutter/material.dart';
+import 'package:notes_app/widget/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body:NotesViewBody() ,
+    );
   }
 }
