a848884b9ab24bb6f93ab39066dce5e9850837ab
fadelnajem <fadel.almadar@gmail.com>
2023-02-22 13:54:49 +0200

    add addcubit to  addnote bottom sheet

---
+++ lib/widget/add_note_form.dart
@@ -0,0 +1,62 @@
+
+import 'package:flutter/material.dart';
+
+import 'custom_botton.dart';
+import 'custom_text_field.dart';
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  String? title, subTitle;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 30,
+          ),
+          CustomTextField(
+            onSaved: (vlaue) {
+              title = vlaue;
+            },
+            hintText: 'Title',
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          CustomTextField(
+            onSaved: (vlaue) {
+              subTitle = vlaue;
+            },
+            hintText: 'Content',
+            maxLines: 5,
+          ),
+          CustomBotton(
+            onTap: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+        ],
+      ),
+    );
+  }
+}
