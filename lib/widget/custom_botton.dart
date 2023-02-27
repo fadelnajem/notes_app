9485ef8143f2ee0f36b7f2fae3bea617cb4ae8fa
fadelnajem <fadel.almadar@gmail.com>
2023-02-27 13:22:56 +0200

    change add note loading

--- lib/widget/custom_botton.dart
+++ lib/widget/custom_botton.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
 
 class CustomBotton extends StatelessWidget {
-  const CustomBotton({super.key, this.onTap});
+  const CustomBotton({super.key, this.onTap, this.isLoading = false});
   final void Function()? onTap;
+  final bool isLoading;
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -17,15 +18,21 @@ class CustomBotton extends StatelessWidget {
           ),
           width: MediaQuery.of(context).size.width,
           height: 55,
-          child: const Center(
-            child: Text(
-              'Add',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
+          child: Center(
+            child: isLoading
+                ?const  SizedBox( height: 30, width: 30,
+                  child:  CircularProgressIndicator(
+                      color: Colors.black,
+                    ),
+                )
+                : const Text(
+                    'Add',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
           ),
         ),
       ),
