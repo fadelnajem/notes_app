8d4c374157acff56f0853226f3d8b1b10f7edc9f
fadelnajem <fadel.almadar@gmail.com>
2023-02-28 12:59:26 +0200

    delete notes okay

--- lib/widget/custom_note_item.dart
+++ lib/widget/custom_note_item.dart
@@ -42,7 +42,9 @@ class NoteItem extends StatelessWidget {
                 ),
               ),
               trailing: IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  notes.delete();
+                },
                 icon: const Icon(
                   FontAwesomeIcons.trash,
                   color: Colors.black,
